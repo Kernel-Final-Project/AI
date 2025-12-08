2d6bcb9ffa037b3cc468b3f2b77f1571007e4221
kimjuno1220 <kjo9577@gmail.com>
2025-12-08 11:37:46 +0900

    content_ai 리팩터링

--- ssadagu_parser/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-싸다구 크롤러 패키지
-"""
-from ssadagu_parser.crawler import crawl_category, crawl_from_main
-from ssadagu_parser.product_extractor import extract_products_from_html
-
-__all__ = ['crawl_category', 'crawl_from_main', 'extract_products_from_html']
-
